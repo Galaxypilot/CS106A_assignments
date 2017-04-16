cffdb79f9626aabda9d331a8d4727bd1f440d238
Gin-Chan <erdongerzong@qq.com>
2017-04-16 23:13:29 +0800

    import assignment4 project

---
+++ Assignment4/Hangman.java
@@ -0,0 +1,25 @@
+/*
+ * File: Hangman.java
+ * ------------------
+ * This program will eventually play the Hangman game from
+ * Assignment #4.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.awt.*;
+
+public class Hangman extends ConsoleProgram {
+
+    public void run() {
+		/* You fill this in */
+    	/**1. Choose a random word*/
+    	
+    	/**2. Keep track of the user¡¯s partially guessed word*/
+    	
+    	
+	}
+
+}
